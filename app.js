6b36e0ca122e4cbda52983767e75d715ec4d3d76
SAAHIL <02saahil@gmail.com>
2022-10-15 17:47:42 +0530

    js imported

---
+++ app.js
@@ -0,0 +1,5 @@
+// console.log("hello World");
+// alert("This script works")
+ var username =prompt("Give me username")
+ var welcomeMessage = "this script works!!  "+ username;
+ alert(welcomeMessage);
