2cc3c4c157e040f084687b0ad484bdf8ffa36c6c
SAAHIL <02saahil@gmail.com>
2022-10-16 12:11:38 +0530

    TAKING INPUT AND CONSOLING

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-// console.log("hello World");
-// alert("This script works")
- var username =prompt("Give me username")
- var welcomeMessage = "this script works!!  "+ username;
- alert(welcomeMessage);
+// // console.log("hello World");
+// // alert("This script works")
+//  var username =prompt("Give me username")
+//  var welcomeMessage = "this script works!!  "+ username;
+//  alert(welcomeMessage);
+var btnTranslate = document.querySelector("#btn-translate");
+// console.log(btnTranslate)
+var txtInput = document.querySelector("#txt-input");
+
+
+btnTranslate.addEventListener("click",  clickHandler)
+
+function clickHandler() {
+    console.log("clicked");
+    console.log("input",txtInput.value);
+    };
+
